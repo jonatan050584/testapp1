b31977dc8154ded19873e63976a49d19ee5c090e
jonatan050584 <jonatan_angelo@hotmail.com>
2014-07-02 17:35:27 -0500

    v4

--- www/exercisetracker.js
+++ www/exercisetracker.js
@@ -1,5 +1,5 @@
 document.addEventListener("deviceready", function(){
-   
+   $("#mensaje").html("que tal");
   if(navigator.network.connection.type == Connection.NONE){
     $("#home_network_button").text('No Internet Access')
                  .attr("data-icon", "delete")
